04424b56daca79b4ed711d054795d32615d60a96
jeremyroques <38661883+jeremyroques@users.noreply.github.com>
2018-04-25 16:39:05 +0200

    Add files via upload
    ajout de Pj et Pnj

--- src/main/java/fr/uvsq/poo/monprojet/Joueur.java
+++ src/main/java/fr/uvsq/poo/monprojet/Joueur.java
@@ -1,33 +1,24 @@
-package fr.uvsq.poo.monprojet;
+package Projet;
 import java.util.*;
 
 
 public class Joueur {
 	int x,y;
 	String dir;
-	int vie;
-	String type;
 	int puissance;
-	ArrayList<Objet> inventaire;
 	
 	/* 	
-	 *	créer un joueur de type PNJ ou PJ en fonction de l'argument
-	 *	positionné aléatoirement dans une matrice salle
+	 *	crï¿½er un joueur de type PNJ ou PJ en fonction de l'argument
+	 *	positionnï¿½ alï¿½atoirement dans une matrice salle
 	 *	vie de base = 3, puissance de base = 30, si PNJ pas de vie
 	 *
 	 */
 	
-	public Joueur(String type) {
+	public Joueur() {
 		x = (int) (Math.random() * (Variables.largeur_salle - 1)) + 1;
 		y = (int) (Math.random() * (Variables.hauteur_salle - 1)) + 1;
-		dir = "up";
-		this.type = type;
-		this.puissance = 30;
-		if(this.type == "PJ") {
-			this.vie = 3;
-			this.inventaire = new ArrayList<Objet>();
-		}
-		else this.vie = 1;		
+		dir = "haut";
+		this.puissance = 0;
 	}
 	
 	public int getPosX() {
@@ -42,28 +33,15 @@ public class Joueur {
 		return this.dir;
 	}
 	
-	/*
-	 *  Commandes en anglais
-	 */
 	public Joueur Move(String dir) {
-		System.out.println(this.getPosX() + " " + this.getPosY());
-		if(dir.equals("down") && this.getPosY() < Variables.hauteur_salle - 1) this.y += 1;
-		else if(dir.equals("up") && this.getPosY() > 0) this.y -= 1;
-		else if(dir.equals("left") && this.getPosX() > 0) this.x -= 1;
-		else if(dir.equals("right") && this.getPosX() < Variables.largeur_salle - 1) this.x += 1;
-		else System.out.println("Erreur de direction");		
+		if(dir == "bas" && this.getPosY() < Variables.hauteur_salle) this.x += 1;
+		else if(dir == "haut" && this.getPosY() > 0) this.x -= 1;
+		else if(dir == "gauche" && this.getPosX() > 0) this.y -= 1;
+		else if(dir == "droite" && this.getPosX() < Variables.largeur_salle) this.y += 1;
+		else System.out.println("erreur de direction");		
 		this.dir = dir;
 		return this;
 	}
 	
-	public Joueur Turn(String dir) {
-		this.dir = dir;
-		return this;
-	}
-	
-	public ArrayList<Objet> PickUp(Objet Obj){
-		 this.inventaire.add(Obj);
-		 return this.inventaire;
-	}
 	
 }
---
+++ src/main/java/fr/uvsq/poo/monprojet/Pnj.java
@@ -0,0 +1,16 @@
+package Projet;
+
+public class Pnj extends Joueur {
+	
+
+	public Pnj() {
+		super();
+	}
+	
+	public Joueur Move(String dir) {
+		super.Move(dir);
+		return this;
+		
+	}
+
+}
